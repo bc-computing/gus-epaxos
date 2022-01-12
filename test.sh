a1b2ec62da4296ab15a3c215c5c516537924d09f
root <root@ms0926.utah.cloudlab.us>
2022-01-11 21:10:07 -0700

    update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-timeout 20s bin/client -maddr=10.10.1.1 -writes=$1 -c=$2 -T=$3 -size=$4 -rampUp=1 -rampDown=0 -singleClusterTest=false
+timeout 40s bin/client -maddr=10.10.1.1 -writes=$1 -c=$2 -T=$3 -size=$4 -rampUp=2 -rampDown=0 -singleClusterTest=true
 #tail -n +200 latFileRead-0.txt > tmp.txt && mv tmp.txt latFileRead-0.txt
 #tail -n +200 latFileRead-1.txt > tmp.txt && mv tmp.txt latFileRead-1.txt
 #tail -n +200 latFileRead-2.txt > tmp.txt && mv tmp.txt latFileRead-2.txt
